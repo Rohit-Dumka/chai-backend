89f6b2bdb6a89b05f3f8cdf17a0f6706bfd186f8
unknown <rohit01dumka@gmail.com>
2025-01-06 20:12:50 +0530

    new commit

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose"
-
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
 const VideoSchema = new Schema(
     {
         videoFile: {
@@ -39,5 +39,7 @@ const VideoSchema = new Schema(
         timestamps: true
     }
 )
+
+VideoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("Video",VideoSchema)
